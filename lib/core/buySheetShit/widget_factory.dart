813270e193cfc6fafb3e5e60f452cb19013f365c
UrsaMedium <celestial.lovelace@gmail.com>
2026-05-01 23:51:47 +0300

    widget-factory-added-radio-and-dropdown-menu

--- lib/core/buySheetShit/widget_factory.dart
+++ lib/core/buySheetShit/widget_factory.dart
@@ -36,8 +36,84 @@ Color widgetPropertyColor(BuildContext context, String color) {
   }
 }
 
+class WhatWidgetRadio extends StatefulWidget {
+  final Map<String, String> radios;
+  const WhatWidgetRadio({super.key, required this.radios});
 
+  @override
+  State<WhatWidgetRadio> createState() => _WhatWidgetRadioState();
+}
 
+class _WhatWidgetRadioState extends State<WhatWidgetRadio> {
+  String _groupValue = 'roo';
+
+  void radioReacts(String? value) {
+    setState(() {
+      _groupValue = value ?? 'err';
+    });
+  }
+
+  List<Widget> choices() {
+    List<Widget> resultList = [];
+    for (final each in widget.radios.keys) {
+      resultList.add(
+        InkWell(
+          onTap: () => radioReacts(widget.radios[each]),
+          child: ListTile(
+            title: Text(widget.radios[each]!),
+            leading: Radio<String>(value: widget.radios[each]!),
+          ),
+        ),
+      );
+    }
+    return resultList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RadioGroup<String>(
+      groupValue: _groupValue,
+      onChanged: (value) {
+        radioReacts(value);
+      },
+      child: Column(children: choices()),
+    );
+  }
+}
+
+class WhatWidgetDropdownMenu extends StatefulWidget {
+  final Map<String, String> entries;
+  const WhatWidgetDropdownMenu({super.key, required this.entries});
+
+  @override
+  State<WhatWidgetDropdownMenu> createState() => _WhatWidgetDropdownMenuState();
+}
+
+class _WhatWidgetDropdownMenuState extends State<WhatWidgetDropdownMenu> {
+  final TextEditingController _selectedItem = TextEditingController();
+  String selected = '';
+
+  List<DropdownMenuEntry<String>> listOfEntries() {
+    List<DropdownMenuEntry<String>> resultList = [];
+    for (final each in widget.entries.keys) {
+      resultList.add(
+        DropdownMenuEntry(value: each, label: widget.entries[each]!),
+      );
+    }
+    return resultList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      onSelected: (value) => setState(() {
+        selected = value!;
+      }),
+      controller: _selectedItem,
+      dropdownMenuEntries: listOfEntries(),
+    );
+  }
+}
 
 Widget theAppWidgetBuilder(
   BuildContext context,
@@ -56,19 +132,25 @@ Widget theAppWidgetBuilder(
             color: widgetPropertyColor(context, widgetProperties['color']!),
             fontSize: double.tryParse(widgetProperties['fontSize'] ?? '14'),
             fontWeight: FontWeight(
-              int.tryParse(widgetProperties['fontWeight'] ?? '400') ?? 400,
+              int.tryParse(widgetProperties['fontWeight']!) ?? 400,
             ),
           ),
         ),
       );
-      case 'TextField':
-        TextEditingController textFieldController = TextEditingController();
-        return TextField(controller: textFieldController, onChanged: (value) {
-           appCartItems[widgetProperties['text'] ?? ''] = textFieldController.toString();
-        },);
-      case 'radio':
-
+    case 'TextField':
+      TextEditingController textFieldController = TextEditingController();
+      return TextField(
+        controller: textFieldController,
+        onChanged: (value) {
+          appCartItems[widgetProperties['text'] ?? ''] = textFieldController
+              .toString();
+        },
+      );
+    case 'radio':
+      return WhatWidgetRadio(radios: widgetProperties);
+    case 'DropdownMenu':
+      return WhatWidgetDropdownMenu(entries: widgetProperties);
     default:
       return Text('ERROR');
   }
-}  
+}
--- lib/views/store_views/store_pages/cart_page_view.dart
+++ lib/views/store_views/store_pages/cart_page_view.dart
@@ -5,10 +5,82 @@ class CartPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('data')
+    return Column(children: [TestRadio(), TestDropDownMenu()]);
+  }
+}
+
+class TestDropDownMenu extends StatefulWidget {
+  const TestDropDownMenu({super.key});
+
+  @override
+  State<TestDropDownMenu> createState() => _TestDropDownMenuState();
+}
+
+class _TestDropDownMenuState extends State<TestDropDownMenu> {
+  final List<String> _entries = ['1', '2', '3', '4'];
+  final TextEditingController _selectedItem = TextEditingController();
+  String _selected = '';
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      onSelected: (value) => setState(() {
+        _selected = value!;
+      }),
+      controller: _selectedItem,
+      dropdownMenuEntries: <DropdownMenuEntry<String>>[
+        DropdownMenuEntry(value: _entries[1], label: _entries[1]),
+        DropdownMenuEntry(value: _entries[2], label: _entries[2]),
+        DropdownMenuEntry(value: _entries[3], label: _entries[3]),
       ],
+    );
+  }
+}
+
+class TestRadio extends StatefulWidget {
+  const TestRadio({super.key});
+
+  @override
+  State<TestRadio> createState() => _TestRadioState();
+}
+
+class _TestRadioState extends State<TestRadio> {
+  String _groupValue = 'roo';
+  Map<String, String> radios = {
+    'roomba': 'roo',
+    'boomba': 'boo',
+    'yoomba': 'yoo',
+  };
+
+  void radioReacts(String? value) {
+    setState(() {
+      _groupValue = value ?? 'err';
+    });
+  }
+
+  List<Widget> choices() {
+    List<Widget> resultList = [];
+    for (final each in radios.keys) {
+      resultList.add(
+        InkWell(
+          onTap: () => radioReacts(radios[each]),
+          child: ListTile(
+            title: Text(radios[each]!),
+            leading: Radio<String>(value: radios[each]!),
+          ),
+        ),
+      );
+    }
+    return resultList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RadioGroup<String>(
+      groupValue: _groupValue,
+      onChanged: (value) {
+        radioReacts(value);
+      },
+      child: Column(children: choices()),
     );
   }
 }
