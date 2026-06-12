f3c18d3a9f4662b70e9ecc9f2786e24db6d22a51
UrsaMedium <celestial.lovelace@gmail.com>
2026-06-12 04:03:36 +0300

    started-creating-outer-binding-inner-circles-for-cart-items

---
+++ lib/core/backend_and_bindings/database/subscribtion_data/product_cart/cart_inner_circle/data_models/cart_item_model.dart
@@ -0,0 +1,37 @@
+class CartItem {
+  final String id;
+  final String userId;
+  final String userName;
+  final String productId;
+  final String productName;
+  final Map<String, String> purchaseData; // field : data from the field
+  final String currency;
+  final double checkoutPrice;
+  final String status;
+  final String? comment;
+
+  CartItem({
+    required this.id,
+    required this.userId,
+    required this.userName,
+    required this.productId,
+    required this.productName,
+    required this.purchaseData,
+    required this.currency,
+    required this.checkoutPrice,
+    required this.status,
+    this.comment,
+  });
+
+  factory CartItem.intial() => CartItem(
+    id: 'id',
+    userId: 'userId',
+    userName: 'userName',
+    productId: 'productId',
+    productName: 'productName',
+    purchaseData: {},
+    currency: 'currency',
+    checkoutPrice: 0,
+    status: 'status',
+  );
+}
