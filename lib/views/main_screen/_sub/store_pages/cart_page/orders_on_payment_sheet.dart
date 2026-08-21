d6a32bfebec5fde9191498bc2bb97b9e583a6203
UrsaMedium <celestial.lovelace@gmail.com>
2026-08-21 18:54:53 +0300

    betta-1.9-added-details-view-for-the-pending-orders

--- lib/views/main_screen/_sub/store_pages/cart_page/orders_on_payment_sheet.dart
+++ lib/views/main_screen/_sub/store_pages/cart_page/orders_on_payment_sheet.dart
@@ -1,9 +1,15 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:crabpay/core/backend/database/general_db/db_inner_circle/data_models/product_model.dart';
+import 'package:crabpay/core/backend/database/general_db/db_inner_circle/database_bloc/database_bloc.dart';
 import 'package:crabpay/core/backend/database/product_cart/cart_inner_circle/cart_bloc/cart_bloc.dart';
+import 'package:crabpay/core/backend/database/product_cart/cart_inner_circle/data_models/cart_item_model.dart';
 import 'package:crabpay/core/backend/database/product_cart/cart_inner_circle/data_models/pending_order_model.dart';
 import 'package:crabpay/core/backend/pyament_services/payment_bloc/payment_bloc.dart';
 import 'package:crabpay/core/backend/pyament_services/payment_bloc/payment_event.dart';
 import 'package:crabpay/core/global_graphic_driver.dart';
+import 'package:crabpay/core/utilities.dart';
 import 'package:crabpay/views/custom_ui_elements/utilities/ui_utilities.dart';
+import 'package:crabpay/views/custom_ui_elements/widgets/material_shimering_place_holder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -18,6 +24,19 @@ class OrdersOnPaymentSheetDriver extends StatelessWidget {
     }
   }
 
+  void _onOrderDetailsPressed(
+    BuildContext context,
+    List<Product> products,
+    PendingOrder order,
+  ) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return _OrderDetails(order: order, products: products);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final pendingOrders = context.select<CartBloc, List<PendingOrder>>(
@@ -27,6 +46,11 @@ class OrdersOnPaymentSheetDriver extends StatelessWidget {
       orders: pendingOrders,
       onPaymentLinkPressed: (paymentLink) =>
           _onPaymentLinkPressed(context, paymentLink),
+      onOrderDetailsPressed: (order) => _onOrderDetailsPressed(
+        context,
+        context.read<DatabaseBloc>().state.products ?? [],
+        order,
+      ),
     );
   }
 }
@@ -34,10 +58,12 @@ class OrdersOnPaymentSheetDriver extends StatelessWidget {
 class MaterialOrdersOnPaymentSheet extends StatelessWidget {
   final Function(String) onPaymentLinkPressed;
   final List<PendingOrder> orders;
+  final Function(PendingOrder) onOrderDetailsPressed;
   const MaterialOrdersOnPaymentSheet({
     super.key,
     required this.onPaymentLinkPressed,
     required this.orders,
+    required this.onOrderDetailsPressed,
   });
 
   @override
@@ -87,10 +113,10 @@ class MaterialOrdersOnPaymentSheet extends StatelessWidget {
                   itemExtent: 100,
                   itemBuilder: (context, index) =>
                       OrderContainerForOrdersOnPaymentSheet(
-                        order: orders[index].paymentId.substring(0, 7),
-                        total: orders[index].totalPrice,
-                        link: orders[index].paymentLink,
+                        order: orders[index],
                         onPaymentLinkPressed: onPaymentLinkPressed,
+                        onDetailsPressed: (order) =>
+                            onOrderDetailsPressed(order),
                       ),
                 ),
               ),
@@ -103,16 +129,14 @@ class MaterialOrdersOnPaymentSheet extends StatelessWidget {
 }
 
 class OrderContainerForOrdersOnPaymentSheet extends StatelessWidget {
-  final String order;
-  final double total;
-  final String link;
+  final PendingOrder order;
   final Function(String) onPaymentLinkPressed;
+  final Function(PendingOrder) onDetailsPressed;
   const OrderContainerForOrdersOnPaymentSheet({
     super.key,
     required this.order,
-    required this.total,
-    required this.link,
     required this.onPaymentLinkPressed,
+    required this.onDetailsPressed,
   });
 
   @override
@@ -135,7 +159,7 @@ class OrderContainerForOrdersOnPaymentSheet extends StatelessWidget {
               spacing: 6,
               children: [
                 Text(
-                  'Order: $order',
+                  'Order: ${order.paymentId.substring(0, 7)}',
                   style: TextStyle(
                     color: context.appColorScheme.onSurface,
                     fontSize: 15,
@@ -143,7 +167,7 @@ class OrderContainerForOrdersOnPaymentSheet extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  'Total: $total',
+                  'Total: ${order.totalPrice}',
                   style: TextStyle(
                     color: context.appColorScheme.onSurface,
                     fontSize: 15,
@@ -152,9 +176,207 @@ class OrderContainerForOrdersOnPaymentSheet extends StatelessWidget {
                 ),
               ],
             ),
-            ElevatedButton(
-              onPressed: () => onPaymentLinkPressed(link),
-              child: Text('Pay'),
+            Row(
+              spacing: 4,
+              children: [
+                ElevatedButton(
+                  onPressed: () => onDetailsPressed(order),
+                  child: Icon(Icons.more_horiz_rounded),
+                ),
+                ElevatedButton(
+                  onPressed: () => onPaymentLinkPressed(order.paymentLink),
+                  child: Text('Pay'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String generateDetails(CartItem cartItem) {
+  String result = '';
+  for (var key in cartItem.purchaseData.keys) {
+    result += '$key: ${cartItem.purchaseData[key]}\n';
+  }
+  return result.trimRight();
+}
+
+class _OrderDetails extends StatelessWidget {
+  final PendingOrder order;
+  final List<Product> products;
+  const _OrderDetails({required this.order, required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: .circular(24)),
+      child: Container(
+        margin: .all(4),
+        height: 360,
+        width: 308,
+        child: Column(
+          spacing: 4,
+          crossAxisAlignment: .start,
+          children: [
+            Card(
+              color: context.appColorScheme.surfaceContainerLowest,
+              margin: .all(0),
+              shape: RoundedRectangleBorder(borderRadius: .circular(20)),
+              child: Container(
+                margin: .all(8),
+                padding: const .symmetric(horizontal: 16, vertical: 4),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: .spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: .start,
+                          children: [
+                            Text(
+                              dateConversion(
+                                order.cartItems.last.createdAt.toString(),
+                              ),
+                              style: const TextStyle(fontWeight: .w600),
+                            ),
+                            Text('Order: ${order.paymentId.substring(0, 7)}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        // horizontal: 16,
+                        vertical: 4,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: .spaceBetween,
+                        children: [
+                          Row(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text('Products'),
+                              Text(' ${order.cartItems.length}'),
+                            ],
+                          ),
+                          Row(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text('Order Price'),
+                              Text(' \$${order.totalPrice}'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: context.appColorScheme.surfaceContainerLow,
+                  borderRadius: .circular(20),
+                ),
+                // height: 244,
+                child: CustomScrollView(
+                  slivers: [
+                    SliverPadding(
+                      padding: .all(12),
+                      sliver: SliverList.builder(
+                        itemCount: order.cartItems.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: .only(bottom: 4),
+                            width: 288,
+                            child: Row(
+                              crossAxisAlignment: .start,
+                              mainAxisAlignment: .spaceBetween,
+                              children: [
+                                Container(
+                                  margin: .only(top: 8),
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                    borderRadius: .circular(8),
+                                  ),
+                                  clipBehavior: .antiAlias,
+                                  child: CachedNetworkImage(
+                                    imageUrl:
+                                        'https://regred-rainbowbridge.ru/crabpay/images/products/${(products.firstWhere((element) => element.id == order.cartItems[index].productId)).image}.png',
+                                    fit: .cover,
+                                    errorWidget: (context, error, stackTrace) =>
+                                        Container(
+                                          color: context
+                                              .appColorScheme
+                                              .onInverseSurface,
+                                          alignment: Alignment.center,
+                                          child: Text('🦀'),
+                                        ),
+                                    placeholder: (context, url) =>
+                                        MaterialShimeringPlaceHolder(
+                                          width: 40,
+                                          height: 40,
+                                          cornerRadius: 8,
+                                          color: context
+                                              .appColorScheme
+                                              .surfaceContainerHigh,
+                                        ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: .symmetric(
+                                      horizontal: 16.0,
+                                      vertical: 4,
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment: .start,
+                                      children: [
+                                        Text(
+                                          order.cartItems[index].productName,
+                                        ),
+                                        Text(
+                                          '${order.cartItems[index].checkoutPrice}',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color:
+                                                context.appColorScheme.primary,
+                                            fontWeight: .w600,
+                                          ),
+                                        ),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                            color: context
+                                                .appColorScheme
+                                                .surfaceContainerHigh,
+                                            borderRadius: .circular(10),
+                                          ),
+                                          padding: .all(4),
+                                          child: Text(
+                                            generateDetails(
+                                              order.cartItems[index],
+                                            ),
+                                            style: TextStyle(fontSize: 12),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
